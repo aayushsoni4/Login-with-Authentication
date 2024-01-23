1ecb24609d3f8a72506da3d7886d52dba68aa5b0
Aayush <aayushsoni424@gmail.com>
2024-01-24 01:35:50 +0530

    Organized the files

--- app/routes.py
+++ app/routes.py
@@ -5,8 +5,7 @@ app.secret_key = 'your_secret_key'
 
 @app.route('/')
 def home():
-    is_activated = session.pop('is_activated', False)
-    return render_template('login.html', is_activated=is_activated)
+    return render_template('home.html')
 
 @app.route('/register', methods=['POST', 'GET'])
 def register():
@@ -15,5 +14,17 @@ def register():
         username = request.form.get('username')
         password = request.form.get('password')
         session['is_activated'] = True
-        return redirect(url_for('home'))
+        session['email'] = email
+        session['username'] = username
+        session['password'] = password
+        return redirect(url_for('login'))
     return render_template('register.html')
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    is_activated = session.pop('is_activated', False)
+    email = session.pop('email', None)
+    username = session.pop('username', None)
+    password = session.pop('password', None)
+    return render_template('home.html', is_activated=is_activated, 
+                            email=email, username=username, password=password)

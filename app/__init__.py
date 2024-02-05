db490f7531ad5c8da7031bebb93a2e13d79c1fd8
Aayush <aayushsoni424@gmail.com>
2024-02-05 21:04:26 +0530

    Add blueprints for user authentication routes in __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,11 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 from flask_mail import Mail
+from flask_sqlalchemy import SQLAlchemy
+
+import pyotp
+import os
+import logging
 
 # Create the Flask app instance
 app = Flask(__name__)
@@ -14,8 +19,23 @@ db = SQLAlchemy(app)
 # Initialize Flask-Mail for sending emails
 mail = Mail(app)
 
-# Import routes and models
-from app import routes, models
+# Initialize Flask-Bcrypt with the Flask app for password hashing
+bcrypt = Bcrypt(app)
+
+# Initialize the TOTP generator with the OTP key from the environment variables
+otp = pyotp.TOTP(os.getenv("otp_key"), interval=300)
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+# Import blueprints
+from app.main.routes import main_bp
+from app.auth.routes import auth_bp
+
+# Register blueprints
+app.register_blueprint(main_bp)
+app.register_blueprint(auth_bp, url_prefix="/auth")
 
 # Create tables in the database if necessary
 with app.app_context():

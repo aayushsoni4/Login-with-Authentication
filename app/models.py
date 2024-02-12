79a021d52ecacc58eb6c77c46417c675d1a41f3a
Aayush <aayushsoni424@gmail.com>
2024-02-12 22:19:18 +0530

    Create Image model

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 from app import db
 from flask_login import UserMixin
+from datetime import datetime
 
 
 class User(UserMixin, db.Model):
@@ -20,6 +21,7 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(255), unique=True, nullable=False)
     password = db.Column(db.String(255), nullable=False)
     is_activated = db.Column(db.Boolean, default=False)
+    profile_image = db.Column(db.LargeBinary)
 
     # Required methods for Flask-Login
     def get_id(self):
@@ -36,3 +38,23 @@ class User(UserMixin, db.Model):
 
     def __repr__(self):
         return f"User(id={self.id}, username={self.username}, email={self.email})"
+
+
+class Image(db.Model):
+    """
+    Image model representing the 'images' table in the database.
+
+    Attributes:
+        id (int): Primary key for the Image model.
+        filename (str): Name of the uploaded image file.
+        data (bytea): Binary data of the uploaded image.
+        user_id (int): Foreign key referencing the user who uploaded the image.
+        uploaded_at (DateTime): Timestamp indicating when the image was uploaded.
+    """
+
+    __tablename__ = "images"
+    id = db.Column(db.Integer, primary_key=True)
+    filename = db.Column(db.String(255), nullable=False)
+    data = db.Column(db.LargeBinary(length=(2**32) - 1), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+    uploaded_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
